660108d38cdb934b6ef4304427e06d8188adceed
kalhorofaizan <kalhorofaizan@gmail.com>
2019-04-18 11:58:20 +0500

    login signup

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import {  Text,StyleSheet, View } from 'react-native'
+import {Container,Content,List,SwipeRow,Button,Icon,ListItem,Left,Body,Thumbnail,Item,Title,Right} from 'native-base'
+import image from '../../assets/drawable-mdpi/item.png'
 export default class Cart extends Component {
     constructor(props){
         super(props);
@@ -8,11 +9,32 @@ export default class Cart extends Component {
     }
   render() {
     return (
-      <View>
-        <Text> Cart </Text>
-      </View>
+        <Container>
+            <Content style={{margin:10}}  >
+                    <SwipeRow
+                        rightOpenValue={-75}
+                        body={
+                           <View style={{flexDirection:'row'}}>
+                               <View>
+                                   <Thumbnail source={image} square large />
+                               </View>
+                               <View style={{marginLeft:10}} >
+                                   <Text style={{fontWeight: 'bold',color:'#000000'}} >Game</Text>
+                                   <Text style={{color:'#00C569'}} >Rs 1500</Text>
+                               </View>
+                           </View>
+                        }
+                        right={
+                            <Button danger onPress={() => alert('Trash')}>
+                                <Icon active name="trash" />
+                            </Button>
+                        }
+                    />
+
+
+            </Content>
+        </Container>
     )
   }
 }
 
-const styles = StyleSheet.create({});
